48868a32011fd491ba0c901122d5ca2ec679c2ee
sujin-create <qsoojinq@naver.com>
2023-05-12 17:44:32 +0900

    boolean type

---
+++ src/main/kotlin/w3school/tutorial/Booleans.kt
@@ -0,0 +1,18 @@
+package w3school.tutorial
+
+fun main(){
+    val isK: Boolean = true
+    val isF: Boolean = false
+    println(isK)
+    println(isF)
+
+    val isK2 = true
+    val isF2 = false
+
+    println( 4 > 5) //false
+
+    val x = 10;
+    println(x == 10)//true
+    println(10 == 13)//false
+
+}
