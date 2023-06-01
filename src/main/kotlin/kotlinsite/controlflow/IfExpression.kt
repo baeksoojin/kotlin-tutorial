f48b974e4ddd3023198402650b8f5f42bcf1f7e0
baeksoojin <qsoojinq@naver.com>
2023-06-02 00:13:35 +0900

    if 표현식

--- src/main/kotlin/kotlinsite/controlflow/IfExpression.kt
+++ src/main/kotlin/kotlinsite/controlflow/IfExpression.kt
@@ -1,5 +1,9 @@
 package kotlinsite.controlflow
 
+enum class Status{
+    Expired, InProgress
+}
+
 fun main(){
 
     /**
@@ -13,4 +17,48 @@ fun main(){
     val maxLimit=1
     var maxOrLimit = if(maxLimit > a) maxLimit else if (a>b) a else b
     println(maxOrLimit)
+
+    /**
+     * 식은 표현식으로 분기 될 수 있으며 if문은 return을 포함하기 때문에 가능하다.
+     * 다만, 값을 반환하거나 변수에 할당하는 경우, else는 필수!
+     */
+
+    val max = if(a>b){
+        print("a>b")
+        a
+    }else{
+        print("b>a")
+        b
+    }
+
+    /**
+     * when의 사용법
+     * when(변수){ 변수의 값 -> 결과} 이때 변수에 else를 사용할 수 있다
+     * 주로 Enum type과 함께 사용한다. 혹은 Boolean 정도?
+     */
+    when(readln().toInt()){
+        1 -> println("x->1")
+        2 -> println("x->2")
+        else -> {
+            print("x는 1도 2도 아님")
+        }
+    }
+
+    val status = when(if(readln() == "Expired") Status.Expired else if(readln() == "InProgress") Status.InProgress else "존재하지 않음"){
+        Status.Expired -> println("만료")
+        Status.InProgress -> println("진행중")
+        else -> {
+            println("다시 입력")
+        }
+    }
+
+    /**
+     *is를 사용해 특정 유형을 체크할 수도 있다.
+     */
+    val x = readln()
+    fun hasPrefix(x: Any) = when(x){
+        is String -> x.startsWith("prefix")
+        else -> false
+    }
+
 }
