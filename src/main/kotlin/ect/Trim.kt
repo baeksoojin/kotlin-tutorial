424e78b7e3bcce54dfce41e978f83116b68d36a2
baeksoojin <qsoojinq@naver.com>
2023-06-07 15:59:32 +0900

    trim

---
+++ src/main/kotlin/ect/Trim.kt
@@ -0,0 +1,13 @@
+package ect
+
+fun main(){
+    val input: String = readln().toString()
+
+    val result = input.trim()
+    print(result)
+
+    val ex = "asdf asdf asdffff"
+    val result2 = ex.trim()
+    print(result2)
+
+}
